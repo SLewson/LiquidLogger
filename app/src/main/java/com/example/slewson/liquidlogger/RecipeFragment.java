6c7af09a0b64f6b2114f20fe187fb722c1259686
Marie Grap <mbgrap@gmail.com>
2015-05-21 11:31:25 -0700

    add parse library

--- app/src/main/java/com/example/slewson/liquidlogger/RecipeFragment.java
+++ app/src/main/java/com/example/slewson/liquidlogger/RecipeFragment.java
@@ -1,24 +1,27 @@
 package com.example.slewson.liquidlogger;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import com.parse.ParseObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by Marie on 5/21/2015.
  */
 public class RecipeFragment extends ListFragment {
+    private ArrayList mValues;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
+
+        initValues();
+        String[] values = (String[]) mValues.toArray(new String[mValues.size()]);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
@@ -26,6 +29,17 @@ public class RecipeFragment extends ListFragment {
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        // TODO implement some logic
+        Toast.makeText(getActivity(),
+                String.valueOf(l.getItemAtPosition(position)),
+                Toast.LENGTH_LONG).show();
+    }
+
+    private void initValues() {
+        mValues = new ArrayList();
+        mValues.add("Add Recipe");
+
+        ParseObject po = null;
+
+        mValues.add("Recipe 1");
     }
 }
