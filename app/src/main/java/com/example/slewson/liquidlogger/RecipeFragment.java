e0cdb6c44a993b9b6edfae97cee36ad55ec46048
Marie Grap <mbgrap@gmail.com>
2015-05-21 10:36:05 -0700

    list fragment for recipes

--- app/src/main/java/com/example/slewson/liquidlogger/RecipeFragment.java
+++ app/src/main/java/com/example/slewson/liquidlogger/RecipeFragment.java
@@ -2,19 +2,30 @@ package com.example.slewson.liquidlogger;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Marie on 5/21/2015.
  */
-public class RecipeFragment extends Fragment {
+public class RecipeFragment extends ListFragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Get the view from fragmenttab2.xml
-        View view = inflater.inflate(R.layout.recipe_layout, container, false);
-        return view;
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO implement some logic
     }
 }
