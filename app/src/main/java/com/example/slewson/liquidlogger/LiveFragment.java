3cbfe21fa11e65fb2b43e622b7bca17da5750441
Marie Grap <mbgrap@gmail.com>
2015-05-22 16:26:13 -0700

    live layout

--- app/src/main/java/com/example/slewson/liquidlogger/LiveFragment.java
+++ app/src/main/java/com/example/slewson/liquidlogger/LiveFragment.java
@@ -1,6 +1,7 @@
 package com.example.slewson.liquidlogger;
 
 import android.app.NotificationManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
@@ -9,9 +10,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.ChartData;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 /**
  * Created by Marie on 5/21/2015.
@@ -21,6 +33,8 @@ public class LiveFragment extends Fragment implements LiquidLogAPI.LiquidLogApiC
 
     private TextView pH_textview = null;
     private TextView temp_textview = null;
+
+    private LineChartView lineChartView = null;
 
     private Timer timer = null;
     private boolean notified = false;
@@ -34,9 +48,43 @@ public class LiveFragment extends Fragment implements LiquidLogAPI.LiquidLogApiC
         startCoffeeRefreshTimer();
         //liquidLogAPI.getCoffeeStatus();
 
-        pH_textview = (TextView) getActivity().findViewById(R.id.ph_value);
-        temp_textview = (TextView) getActivity().findViewById(R.id.temp_value);
+        pH_textview = (TextView) view.findViewById(R.id.ph_value);
+        temp_textview = (TextView) view.findViewById(R.id.temp_value);
+
+        lineChartView = (LineChartView) view.findViewById(R.id.line_chart);
+
+        initChart();
         return view;
+    }
+
+    private void initChart() {
+        List<PointValue> values = new ArrayList<>();
+        values.add(new PointValue(0.0f, 2.0f));
+        values.add(new PointValue(1.5f, 1.5f));
+        values.add(new PointValue(2.7f, 3.7f));
+        values.add(new PointValue(3.2f, 4.2f));
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        line.setStrokeWidth(3);
+        List<Line> lines = new ArrayList<>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis x = Axis.generateAxisFromRange(-4f, 4f, 0.25f);
+        x.setName("Time");
+
+        Axis y = new Axis();
+        y.setName("pH");
+        y.setHasSeparationLine(true);
+        y.setHasLines(true);
+
+        data.setAxisXBottom(x);
+        data.setAxisYLeft(y);
+
+        lineChartView.setLineChartData(data);
     }
 
     private void startCoffeeRefreshTimer() {
